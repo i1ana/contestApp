2aa790207aa3a77a42fe55a97ae63e88f05b0081
i1ana <bachwasbaroque@gmail.com>
2016-02-19 09:56:19 -0700

    morning push

---
+++ main.js
@@ -0,0 +1,20 @@
+//Javascript 
+
+angular.module('mainModule', [])
+
+	angular.module('mainModule')
+		angular.controller('mainController', [$scope, function($scope, $http){
+
+
+
+
+
+
+
+
+
+
+
+
+
+		}])
---
+++ video.js
@@ -0,0 +1,23 @@
+//Youtube Video Factory 
+
+var videos = [
+	{
+		this.name: "name",
+		this.URL: "",
+		this.title: "",
+		this.description: ""
+	}, 
+
+
+
+
+
+
+]
+
+return {
+	name: videoName,
+	URL: videoURL,
+	title: videoTitle,
+	description: videoDescription
+}
